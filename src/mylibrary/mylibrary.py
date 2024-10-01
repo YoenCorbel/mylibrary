a235bfe3ba98fb61caf9106e67808926da0e7a40
YoenCorbel <yoen.corbel.vivas@gmail.com>
2024-10-01 10:40:38 +0200

    feat(mylibrary): added a new function

--- src/mylibrary/mylibrary.py
+++ src/mylibrary/mylibrary.py
@@ -1,3 +1,7 @@
 def hello_world():
     "Return the string 'Hello, World !'"
     return "Hello, World !!"
+
+def hello_world2():
+    "Return the string 'Hello, World !'"
+    return "Hello, World !!"
